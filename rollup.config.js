1f2426ed297e6078a12749b1cb13835f8f48f3b3
Ivan Chebykin <45231817+doechon@users.noreply.github.com>
2022-06-16 21:35:37 +0300

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: "js/app.js",
+  input: "ts/app.ts",
   output: { file: "bundle.js" },
   plugins: [resolve({ browser: true }), terser()],
 };
