f0c088a15549c5f7b3048d67386868a9c2237780
Ivan Chebykin <45231817+doechon@users.noreply.github.com>
2022-06-23 20:40:25 +0300

    add bundle and related files

--- bundle.js
+++ bundle.js
@@ -1,6 +1,1 @@
-$.getJSON("https://favqs.com/api/qotd", function (data) {
-  let author = document.querySelector(".quote__author");
-  let quote = document.querySelector(".quote__body");
-  author.textContent = data.quote.author;
-  quote.textContent = data.quote.body;
-});
+$.getJSON("https://favqs.com/api/qotd",(function(t){var e=document.querySelector(".quote__author"),o=document.querySelector(".quote__body");e.textContent=t.quote.author,o.textContent=t.quote.body}));
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,18 @@
-export default {
-    input: 'js/app.js',
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+import typescript from '@rollup/plugin-typescript';
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const obj = {
+    input: 'ts/app.ts',
     output: { file: 'bundle.js' },
-};
+    plugins: [
+        nodeResolve({ browser: true }),
+        typescript(),
+        terser(),
+    ]
+}
+
+export default obj;
