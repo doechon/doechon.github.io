7e65f729703d50cdaf8c2350b3c0906783cf25f2
Ivan Chebykin <45231817+doechon@users.noreply.github.com>
2022-06-24 00:50:29 +0300

    delete old folder

--- old/bundle.js
+++
@@ -1,1 +0,0 @@
-$.getJSON("https://favqs.com/api/qotd",(function(t){var e=document.querySelector(".quote__author"),o=document.querySelector(".quote__body");e.textContent=t.quote.author,o.textContent=t.quote.body}));
--- old/ts/app.ts
+++
@@ -1,6 +0,0 @@
-// $.getJSON("https://favqs.com/api/qotd", function (data) {
-//   let author = document.querySelector(".quote__author") as HTMLElement;
-//   let quote = document.querySelector(".quote__body") as HTMLElement;
-//   author.textContent = data.quote.author;
-//   quote.textContent = data.quote.body;
-// });
