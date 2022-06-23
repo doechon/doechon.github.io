a8503fc7e2984ab62ecec6734a9a0da0c8e2c969
Ivan Chebykin <45231817+doechon@users.noreply.github.com>
2022-06-24 00:43:49 +0300

    add working api

--- old/ts/app.ts
+++ old/ts/app.ts
@@ -1,6 +1,6 @@
-$.getJSON("https://favqs.com/api/qotd", function (data) {
-  let author = document.querySelector(".quote__author") as HTMLElement;
-  let quote = document.querySelector(".quote__body") as HTMLElement;
-  author.textContent = data.quote.author;
-  quote.textContent = data.quote.body;
-});
+// $.getJSON("https://favqs.com/api/qotd", function (data) {
+//   let author = document.querySelector(".quote__author") as HTMLElement;
+//   let quote = document.querySelector(".quote__body") as HTMLElement;
+//   author.textContent = data.quote.author;
+//   quote.textContent = data.quote.body;
+// });
