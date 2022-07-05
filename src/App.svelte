c79a2bb4ec19e130613b7e8513f1d18678b257eb
Ivan Chebykin <45231817+doechon@users.noreply.github.com>
2022-07-05 18:24:49 +0300

    divide style css into coressponding components

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@ import MyProjects from './Skeleton/Main/MyProjects.svelte';
 import ContactMe from './Skeleton/Main/ContactMe.svelte';
 import Quote from './Skeleton/Main/Quote.svelte';
 import Fotter from "./Skeleton/Fotter.svelte";
-import Style from "./Style.svelte";
+// import Style from "./Style.svelte";
 </script>
 <main>
 	<Header/>
@@ -17,3 +17,14 @@ import Style from "./Style.svelte";
 	</div>
 	<Fotter/>
 </main>
+<style>
+	:global(body){
+        /* background-color: lightseagreen; */
+        background-image: url("../img/background.jpg");
+    }
+	    main {
+  font-family: "Roboto", sans-serif;
+  margin: auto;
+  max-width: 750px;
+}
+</style>
